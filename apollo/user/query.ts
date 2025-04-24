07f00d56639de785f8eaa2223ff614f37437470e
IamMukhammed <abdullaevazizkhan@gmail.com>
2025-04-24 12:44:36 -0500

    feat: Develop TrendProperties components based on react query data

--- apollo/user/query.ts
+++ apollo/user/query.ts
@@ -42,41 +42,41 @@ export const GET_AGENTS = gql`
 	}
 `;
 
-export const GET_MEMBER = gql(`
-query GetMember($input: String!) {
-    getMember(memberId: $input) {
-        _id
-        memberType
-        memberStatus
-        memberAuthType
-        memberPhone
-        memberNick
-        memberFullName
-        memberImage
-        memberAddress
-        memberDesc
-        memberProperties
-        memberArticles
-        memberPoints
-        memberLikes
-        memberViews
-        memberFollowings
-				memberFollowers
-        memberRank
-        memberWarnings
-        memberBlocks
-        deletedAt
-        createdAt
-        updatedAt
-        accessToken
-        meFollowed {
-					followingId
-					followerId
-					myFollowing
-				}
-    }
-}
-`);
+export const GET_MEMBER = gql`
+	query GetMember($input: String!) {
+    	getMember(memberId: $input) {
+        	_id
+        	memberType
+        	memberStatus
+        	memberAuthType
+        	memberPhone
+        	memberNick
+        	memberFullName
+        	memberImage
+        	memberAddress
+        	memberDesc
+        	memberProperties
+        	memberArticles
+        	memberPoints
+        	memberLikes
+        	memberViews
+        	memberFollowings
+			memberFollowers
+        	memberRank
+        	memberWarnings
+        	memberBlocks
+        	deletedAt
+        	createdAt
+        	updatedAt
+        	accessToken
+        	meFollowed{
+				followingId
+				followerId
+				myFollowing
+			}
+    	}
+	}
+`;
 
 /**************************
  *        PROPERTY        *
