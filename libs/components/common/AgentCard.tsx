3392244a20f96c9caa7887b2ebe646c70df3d26e
IamMukhammed <abdullaevazizkhan@gmail.com>
2025-05-20 23:28:27 -0500

    fix: Modify Agents list

--- libs/components/common/AgentCard.tsx
+++ libs/components/common/AgentCard.tsx
@@ -44,7 +44,7 @@ const AgentCard = (props: AgentCardProps) => {
 							backgroundRepeat: 'no-repeat',
 						}}
 					>
-						<div>{agent?.Products} products</div>
+						<div>{agent?.memberProducts} products</div>
 					</Box>
 				</Link>
 
